8f1f08d039aa2342e057f6a0c8ce4a4b00fc951d
fed-cap24 <FedericoAgustinCaputo24@gmail.com>
2023-08-25 18:16:27 -0300

    Updated Dynamic Weather LUA
    Updated Dynamic Weather channel, so it adds appropriate IDs to each sound. Each ID is separated by +, so they may then be split by them, and played one after another.

--- lua/server/radio/ISWeatherChannel.lua
+++ lua/server/radio/ISWeatherChannel.lua
@@ -1,6 +1,7 @@
 --***********************************************************
 --**                    THE INDIE STONE                    **
 --**				  Author: turbotutone				   **
+--**               Modded By: Fed_Cap24                    **
 --***********************************************************
 
 WeatherChannel = {};
@@ -74,15 +75,15 @@ function WeatherChannel.Init()
     };
 
     zones = {
-        { name = getRadioText("AEBS_zone_name_s"), sectors = { 2, 5, 8, 9 } },
-        { name = getRadioText("AEBS_zone_name_sw"), sectors = { 1, 3, 6, 7 } },
-        { name = getRadioText("AEBS_zone_name_nw"), sectors = { 10, 14, 15, 18 } },
-        { name = getRadioText("AEBS_zone_name_c"), sectors = { 11, 12, 13, 19 } },
-        { name = getRadioText("AEBS_zone_name_n"), sectors = { 17, 4, 16, 23 } },
-        { name = getRadioText("AEBS_zone_name_ne"), sectors = { 21, 25, 29, 31 } },
-        { name = getRadioText("AEBS_zone_name_w"), sectors = { 22, 24, 28, 32 } },
-        { name = getRadioText("AEBS_zone_name_e"), sectors = { 27, 30, 33, 36 } },
-        { name = getRadioText("AEBS_zone_name_se"), sectors = { 20, 26, 34, 35 } },
+        { name = getRadioText("AEBS_zone_name_s"), sectors = { 2, 5, 8, 9 } , code="AEBS_zone_name_s"},
+        { name = getRadioText("AEBS_zone_name_sw"), sectors = { 1, 3, 6, 7 } , code="AEBS_zone_name_sw"},
+        { name = getRadioText("AEBS_zone_name_nw"), sectors = { 10, 14, 15, 18 } , code="AEBS_zone_name_nw"},
+        { name = getRadioText("AEBS_zone_name_c"), sectors = { 11, 12, 13, 19 } , code="AEBS_zone_name_c"},
+        { name = getRadioText("AEBS_zone_name_n"), sectors = { 17, 4, 16, 23 } , code="AEBS_zone_name_n"},
+        { name = getRadioText("AEBS_zone_name_ne"), sectors = { 21, 25, 29, 31 } , code="AEBS_zone_name_ne"},
+        { name = getRadioText("AEBS_zone_name_w"), sectors = { 22, 24, 28, 32 } , code="AEBS_zone_name_w"},
+        { name = getRadioText("AEBS_zone_name_e"), sectors = { 27, 30, 33, 36 } , code="AEBS_zone_name_e"},
+        { name = getRadioText("AEBS_zone_name_se"), sectors = { 20, 26, 34, 35 } , code="AEBS_zone_name_se"},
     }
 end
 
@@ -199,36 +200,41 @@ end
 --Here it Sets up the forecast string, Be careful, GUID: should have + signs, for multiple Clips.
 
 function WeatherChannel.AddForecast(_c, _bc, _forecast, _prefix, _doFog)
+    local fx = _prefix.."+";
     local s = _prefix;
-    s = s .. WeatherChannel.GetForecastString(1, _forecast);
-    _bc:AddRadioLine( RadioLine.new(comp(s), _c.r, _c.g, _c.b) );
+    aux_s, aux_fx = WeatherChannel.GetForecastString(1, _forecast);
+    fx = fx .. aux_fx
+    s = s .. aux_s
+    _bc:AddRadioLine( RadioLine.new(comp(s), _c.r, _c.g, _c.b,"GUID:"..fx) );
 
-    s = WeatherChannel.GetForecastString(2, _forecast);
-    _bc:AddRadioLine( RadioLine.new(comp(s), _c.r, _c.g, _c.b) );
+    s , fx= WeatherChannel.GetForecastString(2, _forecast);
+    _bc:AddRadioLine( RadioLine.new(comp(s), _c.r, _c.g, _c.b,"GUID:"..fx) );
 
     if _doFog and _forecast:isHasFog() then
-        s = WeatherChannel.GetForecastString(3, _forecast);
-        _bc:AddRadioLine( RadioLine.new(comp(s), _c.r, _c.g, _c.b) );
+        s , fx= WeatherChannel.GetForecastString(3, _forecast);
+        _bc:AddRadioLine( RadioLine.new(comp(s), _c.r, _c.g, _c.b,"GUID:"..fx) );
     end
 
     if _forecast:isWeatherStarts() then
         -- a new weather period starts
-        s = WeatherChannel.GetForecastString(4, _forecast);
-        _bc:AddRadioLine( RadioLine.new(comp(s), _c.r, _c.g, _c.b) );
+        s , fx= WeatherChannel.GetForecastString(4, _forecast);
+        _bc:AddRadioLine( RadioLine.new(comp(s), _c.r, _c.g, _c.b,"GUID:"..fx) );
     elseif _forecast:getWeatherOverlap() then
         -- a already started weather period overlaps this day
-        s = WeatherChannel.GetForecastString(5, _forecast);
-        _bc:AddRadioLine( RadioLine.new(comp(s), _c.r, _c.g, _c.b) );
+        s , fx= WeatherChannel.GetForecastString(5, _forecast);
+        _bc:AddRadioLine( RadioLine.new(comp(s), _c.r, _c.g, _c.b,"GUID:"..fx) );
     end
 end
 
 function WeatherChannel.GetForecastString(_type, _forecast)
+    local fx = "";
     local s = "";
     if _type==1 then
         local v = _forecast:getTemperature();
         local a,b,c = v:getTotalMean(), v:getTotalMin(), v:getTotalMax();
         local d = roundstring100(_forecast:getHumidity():getTotalMean()*100);
         s = string.format(" "..getRadioText("AEBS_temperature"), Temperature.getTemperatureString(a), Temperature.getTemperatureString(b), Temperature.getTemperatureString(c), d);
+        fx = string.format("AEBS_temperature_1+VoicedTemp_%s+AEBS_temperature_2+VoicedTemp_%s+AEBS_temperature_3+VoicedTemp_%s+AEBS_temperature_4+VoicedPercent_%s",Temperature.getTemperatureString(a), Temperature.getTemperatureString(b), Temperature.getTemperatureString(c), d);
         --[[
         elseif _type==2 then
             local v = _forecast:getWindPower();
@@ -264,90 +270,119 @@ function WeatherChannel.GetForecastString(_type, _forecast)
         --a = roundstring(ClimateManager.ToKph(a)).." KpH";
         --b = roundstring(ClimateManager.ToKph(b)).." KpH";
         --c = roundstring(ClimateManager.ToKph(c)).." KpH";
+        local wind_speed_code = "";
         if getCore():getOptionDisplayAsCelsius() then
             c = roundstring(ClimateManager.ToKph(c)).." KpH";
+            wind_speed_code = roundstring(ClimateManager.ToKph(c)).."_KpH"
         else
             c = roundstring(ClimateManager.ToMph(c)).." MpH";
+            wind_speed_code = roundstring(ClimateManager.ToMph(c)).."_MpH"
         end
         local d = _forecast:getMeanWindAngleString();
         local dnew = getRadioText("AEBS_zone_name_"..d:lower());
+        local wind_dir_code = "";
         if dnew then
-            d = dnew;
+            wind_dir_code = "AEBS_zone_name_"..d:lower().."+";
+            d = dnew;   
         end
         local e = getRadioText("AEBS_clouds_0");
+        local clouds_code = "AEBS_clouds_0";
         local cloudsA = _forecast:getCloudiness():getTotalMean();
         local cloudsB = _forecast:getCloudiness():getTotalMax();
 
         if cloudsA>0.7 then
             e = getRadioText("AEBS_clouds_2");
+            clouds_code = "AEBS_clouds_2";
         elseif cloudsA>0.4 then
             e = getRadioText("AEBS_clouds_1");
+            clouds_code = "AEBS_clouds_1";
             if cloudsB>0.7 then
                 e = e .. " "..getRadioText("AEBS_clouds_4");
+                clouds_code = clouds_code .. "+".."AEBS_clouds_4";
             end
         else
             if cloudsB>0.7 then
                 e = e .. " "..getRadioText("AEBS_clouds_4");
+                clouds_code = clouds_code .. "+".."AEBS_clouds_4";
             elseif cloudsB>0.4 then
                 e = e .. " "..getRadioText("AEBS_clouds_3");
+                clouds_code = clouds_code .. "+".."AEBS_clouds_3";
             end
         end
 
         local w = getRadioText("AEBS_wind_1");
+        local wind_strength_code = "AEBS_wind_1";
         if a>0.75 then
             w = getRadioText("AEBS_wind_4");
+            wind_strength_code = "AEBS_wind_4";
         elseif a>0.5 then
             w = getRadioText("AEBS_wind_3");
+            wind_strength_code = "AEBS_wind_3";
         elseif a>0.25 then
             w = getRadioText("AEBS_wind_2");
+            wind_strength_code = "AEBS_wind_2";
         end
 
         s = string.format(getRadioText("AEBS_wind_0"), w, d, c, e);
+        fx = string.format("%s+AEBS_wind_0_1+%sAEBS_wind_0_2+%s+AEBS_wind_0_3+%s",wind_strength_code,wind_dir_code,wind_speed_code,clouds_code);
         --s = string.format("Wind speed mean %s, min %s, max %s, average direction %s... %s", a, b, c, d, e);
     elseif _type==3 then
         local v = _forecast:getFogStrength();
         if v==1 then
             s = getRadioText("AEBS_fog_2");
+            fx = "AEBS_fog_2";
         elseif v>0.75 then
             s = getRadioText("AEBS_fog_1");
+            fx = "AEBS_fog_1";
         else
             s = getRadioText("AEBS_fog_0");
+            fx = "AEBS_fog_0";
         end
     elseif _type==4 or _type==5 then
         --local hour = _gametime:getHour();
         if _type==4 then
             --s = string.format(getRadioText("AEBS_weather_0_a"), tostring(ISDebugUtils.roundNum(_forecast:getWeatherStartTime(),0)));
             s = string.format(getRadioText("AEBS_weather_0_a"), WeatherChannel.GetDaySegmentForHour(_forecast:getWeatherStartTime()));
+            fx = "AEBS_weather_0_a+"..WeatherChannel.GetDaySegmentCodeForHour(_forecast:getWeatherStartTime())
         else
             local endTime = _forecast:getWeatherEndTime();
             if endTime>=22 then
                 s = getRadioText("AEBS_weather_0_b");
+                fx = "AEBS_weather_0_b";
             else
                 --s = string.format(getRadioText("AEBS_weather_0_c"), tostring(ISDebugUtils.roundNum(endTime,0)));
                 s = string.format(getRadioText("AEBS_weather_0_c"), WeatherChannel.GetDaySegmentForHour(endTime));
+                fx = "AEBS_weather_0_c+"..WeatherChannel.GetDaySegmentCodeForHour(endTime);
             end
 
         end
 
         local t = {};
+        local t_fx = {};
         if _forecast:isHasHeavyRain() then
             table.insert(t,getRadioText("AEBS_weather_heavy_rain"));
+            table.insert(t_fx,"AEBS_weather_heavy_rain");
         end
         if _forecast:isHasStorm() then
             table.insert(t,getRadioText("AEBS_weather_storm"));
+            table.insert(t_fx,"AEBS_weather_storm");
         end
         if _forecast:isHasTropicalStorm() then
             table.insert(t,getRadioText("AEBS_weather_tropical"));
+            table.insert(t_fx,"AEBS_weather_tropical");
         end
         if _forecast:isHasBlizzard() then
             table.insert(t,getRadioText("AEBS_weather_blizzard"));
+            table.insert(t_fx,"AEBS_weather_blizzard");
         end
 
         if #t>0 then
             if #t==1 then
                 s = s .. getRadioText("AEBS_weather_predicted")..t[1];
+                fx = fx .. "+AEBS_weather_predicted+"..t_fx[1];
             else
                 s = s .. getRadioText("AEBS_weather_predicted");
+                fx = fx .. "+AEBS_weather_predicted"
                 for k,v in ipairs(t) do
                     if k<#t then
                         s = s .. v .. (v~=#t-1 and ", " or "");
@@ -355,16 +390,25 @@ function WeatherChannel.GetForecastString(_type, _forecast)
                         s = s .. getRadioText("AEBS_weather_and_a") .. v .. "...";
                     end
                 end
+                for k,v in ipairs(t_fx) do
+                    if k<#t_fx then
+                        fx = fx .. "+".. v;
+                    else
+                        fx = fx .. "+AEBS_weather_and_a+" .. v;
+                    end
+                end
             end
         else
             s = s .. getRadioText("AEBS_weather_light_moderate");
+            fx = fx .. "+AEBS_weather_light_moderate"
         end
 
         if _forecast:isChanceOnSnow() then
             s = s..getRadioText("AEBS_weather_snowfall");
+            fx = "+AEBS_weather_snowfall"
         end
     end
-    return s;
+    return s,fx;
 end
 
 function WeatherChannel.GetDaySegmentForHour(_hour)
@@ -381,6 +425,19 @@ function WeatherChannel.GetDaySegmentForHour(_hour)
     end
 end
 
+function WeatherChannel.GetDaySegmentCodeForHour(_hour)
+    if _hour<=4 or _hour>=23 then
+        return "AEBS_segment_night";
+    elseif _hour>=4 and _hour<8 then
+        return "AEBS_segment_early_morning";
+    elseif _hour>=8 and _hour<12 then
+        return "AEBS_segment_morning";
+    elseif _hour>=12 and _hour<18 then
+        return "AEBS_segment_afternoon";
+    elseif _hour>=18 and _hour<23 then
+        return "AEBS_segment_evening";
+    end
+end
 
 function WeatherChannel.AddExtremesForecasting(_c, _bc, offset, _len)
     local clim = getClimateManager();
@@ -391,14 +448,18 @@ function WeatherChannel.AddExtremesForecasting(_c, _bc, offset, _len)
 
         if forecast and ( forecast:isHasBlizzard() or forecast:isHasTropicalStorm() or forecast:isHasStorm() ) then
             local type = getRadioText("AEBS_weather_storm_C");
+            local weather_code = "AEBS_weather_storm_C";
             if forecast:isHasTropicalStorm() then
                 type = getRadioText("AEBS_weather_tropical_C");
+                weather_code = "AEBS_weather_tropical_C";
             elseif forecast:isHasBlizzard() then
                 type = getRadioText("AEBS_weather_blizzard_C");
+                weather_code = "AEBS_weather_blizzard_C";
             end
 
             local s = string.format(getRadioText("AEBS_weather_warning"), type, tostring(i));
-            _bc:AddRadioLine( RadioLine.new(comp(s), _c.r, _c.g, _c.b) );
+            local fx = "AEBS_weather_warning_1+"..weather_code.."+AEBS_weather_warning_2+VoicedNumber_"..tostring(i).."+AEBS_weather_warning_3"
+            _bc:AddRadioLine( RadioLine.new(comp(s), _c.r, _c.g, _c.b,"GUID:"..fx) );
             return;
         end
     end
@@ -414,42 +475,63 @@ function WeatherChannel.GetRandomString(_c, _bc, _doItThreshold, _forceRand)
     local rand = _forceRand or ZombRand(1,10000);
 
     local s = nil;
+    local fx = nil;
     if rand<500 then
         local zone = zones[ZombRand(1,#zones)];
         s = string.format(getRadioText("AEBS_random_0"), zone.name, zone.sectors[1], zone.sectors[2], zone.sectors[3], zone.sectors[4]);
+        fx = "AEBS_random_0_1+"..zone.code.."+AEBS_random_0_2+VoicedNumber_"..tostring(zone.sectors[1]).."+VoicedNumber_"..tostring(zone.sectors[2]).."+VoicedNumber_"..tostring(zone.sectors[3]).."+AEBS_random_0_3+VoicedNumber_"....tostring(zone.sectors[4]);
     elseif rand<1000 then
-        s = string.format(getRadioText("AEBS_random_1"), ZombRand(1,36) );
+        local i=ZombRand(1,36);
+        s = string.format(getRadioText("AEBS_random_1"), i );
+        fx = "AEBS_random_1_1+VoicedNumber_"..tostring(i).."+AEBS_random_1_2";
     elseif rand<1500 then
-        s = string.format(getRadioText("AEBS_random_2"), ZombRand(1,1000) );
+        local i = ZombRand(1,1000);
+        s = string.format(getRadioText("AEBS_random_2"), i );
+        fx = "AEBS_random_2_1+VoicedNumber_"..tostring(i).."+AEBS_random_2_2";
     elseif rand<2000 then
-        s = string.format(getRadioText("AEBS_random_3"), activity[ZombRand(1,#activity)], ZombRand(1,36) );
+        local act_num=ZombRand(1,#activity);
+        local sect=ZombRand(1,36);
+        s = string.format(getRadioText("AEBS_random_3"), activity[act_num], sect );
+        if act_num >= 3 then
+            act_num = act_num + 1; --Activity number 3 is disabled
+        end
+        fx = "AEBS_rand_pre_"..tostring(act_num).."+AEBS_random_3+VoicedNumber_"..tostring(sect); 
     --elseif rand==9000 then
     --    s = getRadioText("AEBS_random_4");
     elseif rand>=9000 and rand<9002 then
         s = getRadioText("AEBS_random_5");
+        fx = "AEBS_random_5";
     elseif rand>=9002 and rand<9004 then
         s = getRadioText("AEBS_random_6");
+        fx = "AEBS_random_6";
     elseif rand>=9004 and rand<9006 then
         s = getRadioText("AEBS_random_7");
+        fx = "AEBS_random_7";
     elseif rand>=9006 and rand<9008 then
         s = getRadioText("AEBS_random_8");
+        fx = "AEBS_random_8";
     elseif rand>=9008 and rand<9010 then
         s = getRadioText("AEBS_random_9");
+        fx = "AEBS_random_9";
     elseif rand>=9010 and rand<9012 then
         s = getRadioText("AEBS_random_10");
+        fx = "AEBS_random_10";
     elseif rand>=9012 and rand<9014 then
         s = getRadioText("AEBS_random_11");
+        fx = "AEBS_random_11";
     elseif rand>=9014 and rand<9016 then
         s = getRadioText("AEBS_random_12");
+        fx = "AEBS_random_12";
     elseif rand>=9016 and rand<9018 then
         s = getRadioText("AEBS_random_13");
+        fx = "AEBS_random_13";
     end
 
     if s~=nil then
         --local radio = getZomboidRadio();
         --s = radio:scrambleString(s, 20, false, nil);
         local c = {r=0.5,g=0.5,b=0.5};
-        _bc:AddRadioLine( RadioLine.new(s, c.r, c.g, c.b) );
+        _bc:AddRadioLine( RadioLine.new(s, c.r, c.g, c.b,"GUID:"..fx) );
     end
 end
 
