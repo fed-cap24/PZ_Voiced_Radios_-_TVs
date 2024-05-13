80482e3f75bb8ff13eae8131dc0cc7dfff21c16a
fed-cap24 <FedericoAgustinCaputo24@gmail.com>
2024-05-13 18:29:34 -0300

    Added Dr. Oids Advert

--- lua/client/RadioCom/VRTVs_SoundTable.lua
+++ lua/client/RadioCom/VRTVs_SoundTable.lua
@@ -192,3 +192,4 @@ RadioWavs.addSongs("1ccbd6ed-40a5-4659-b6bf-2f35f0915dde","465ef67d-21a8-4c22-a6
 RadioWavs.addSongs("84247087-4754-4a2d-a638-3abe0a01e077","7cb6038a-1f23-440a-ad95-68d01a657885")
 RadioWavs.addSongs("889e63b4-cbd1-40e3-b02a-7e3f9a2b5be4","a06dd29c-78cf-4a0b-bba9-600a517bf4a4")
 RadioWavs.addSongs("f5969765-ff22-4bff-9d35-05c5a5bb8098","aeb5fdd9-c5cf-4816-9eea-b8f11daec903")
+RadioWavs.addSongs("d5dfe630-bb8c-48b2-a706-7e2ffa6aa834","9ecd1200-8ead-4e2b-bb7a-9efc422ff77a")
